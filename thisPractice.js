5c2912b3c7bf1549bc62028b3c4c463e0eee32ab
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-10-01 03:25:19 +0500

    More practice on prototype

--- thisPractice.js
+++ thisPractice.js
@@ -1,26 +1,36 @@
-// // const arrowFunc = () => {
-// //   console.log(this);
-// // };
-// // arrowFunc(); // Arrow Function refers to parent context
+// // // const arrowFunc = () => {
+// // //   console.log(this);
+// // // };
+// // // arrowFunc(); // Arrow Function refers to parent context
 
-// // const myObj = {
-// //   username: "Ahmad Sohail",
-// //   funcInsideObject: () => {
-// //     console.log(this);
-// //   },
-// // };
+// // // const myObj = {
+// // //   username: "Ahmad Sohail",
+// // //   funcInsideObject: () => {
+// // //     console.log(this);
+// // //   },
+// // // };
 
-// // myObj.funcInsideObject();
-// "use strict";
+// // // myObj.funcInsideObject();
+// // "use strict";
+// // function sayHello() {
+// //   this.myFriendName = "Hammad";
+// // }
+// // const gh = new sayHello();
+// // sayHello.hi = "Hi";
+// // console.log(gh.myFriendName);
+
+// // const newObj = {
+// //   username: "Ahmad",
+// //   learningFrom: "Chai Aur Code",
+// // };
+// // const gf = new newObj(); // gives an error
+
 // function sayHello() {
-//   this.myFriendName = "Hammad";
+//   console.log("Hi");
 // }
-// const gh = new sayHello();
-// sayHello.hi = "Hi";
-// console.log(gh.myFriendName);
+// Object.prototype.sayAhmad = function () {
+//   console.log("Hi, Ahmad is called from function");
+// };
 
-const newObj = {
-  username: "Ahmad",
-  learningFrom: "Chai Aur Code",
-};
-const gf = new newObj(); // gives an error
+// const myName = sayHello();
+// myName.sayAhmad();
