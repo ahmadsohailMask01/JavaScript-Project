4e7d1a0f29d81d011b845c29af24e1274f233286
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-09-30 11:45:23 +0500

    some practice of this and new

---
+++ thisPractice.js
@@ -0,0 +1,26 @@
+// // const arrowFunc = () => {
+// //   console.log(this);
+// // };
+// // arrowFunc(); // Arrow Function refers to parent context
+
+// // const myObj = {
+// //   username: "Ahmad Sohail",
+// //   funcInsideObject: () => {
+// //     console.log(this);
+// //   },
+// // };
+
+// // myObj.funcInsideObject();
+// "use strict";
+// function sayHello() {
+//   this.myFriendName = "Hammad";
+// }
+// const gh = new sayHello();
+// sayHello.hi = "Hi";
+// console.log(gh.myFriendName);
+
+const newObj = {
+  username: "Ahmad",
+  learningFrom: "Chai Aur Code",
+};
+const gf = new newObj(); // gives an error
